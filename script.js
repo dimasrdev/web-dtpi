2762bc7f517de20bc625f54a36f07c8ff7bc2e90
dimasrdev <113432596+dimasrdev@users.noreply.github.com>
2022-12-07 10:30:50 +0700

    Masukin kodingan
    Saya ubah halaman depan tampilan

---
+++ script.js
@@ -0,0 +1,12 @@
+let nama = "Dimas";
+let umur = 20;
+
+let button = document.querySelector('button');
+let background = document.querySelector('body');
+let h2 = document.querySelector('h2');
+
+let text = document.getElementById('tulisan');
+
+function ubahBg(){
+	text.innerHTML = "Dimas Rama"
+}
